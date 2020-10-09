bc73739290624784bfbb5e6513af61189f60c027
kentldolar <kentldolar@gmail.com>
2020-10-09 14:56:09 +0900

    {} remove

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami = "ami-acd005d5"
   instance_type = "t2.micro"
-  security_groups = ["${aws_security_group.allow_all.name}"]
+  security_groups = ["aws_security_group.allow_all.name"]
   tags = {
     Name = "IAAC"
   }
